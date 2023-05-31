f07f245f10fcaa228d8d0a3d877d58c0f400eae7
william <william_swl@163.com>
2023-05-31 14:29:58 +0800

    feat: init

---
+++ R/func.R
@@ -0,0 +1,12 @@
+#' Hello rpkg
+#'
+#' @param rpkg package name
+#'
+#' @return rpkg character
+#' @export
+#'
+#' @examples hello_rpkg("rpkg.template")
+hello_rpkg <- function(rpkg) {
+  print(rpkg)
+  return(rpkg)
+}
---
+++ tests/testthat/test-func.R
@@ -0,0 +1,3 @@
+test_that("Hello rpkg", {
+  expect_identical(hello_rpkg("rpkg.template"), "rpkg.template")
+})
